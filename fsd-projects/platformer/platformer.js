07035ad568e37d234e3fadccc628ee5f3e2bef31
Jas Kennedy <jaskennedy5@outlook.com>
2026-02-24 15:02:55 +0000

    update

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,24 +27,42 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
 
+    toggleGrid();
 
     // TODO 2 - Create Platforms
-
-
-
+    //createPlatform(xPos, yPos, Width, Height, "Color")
+    createPlatform(300, 620, 100, 25);
+    createPlatform(600, 555, 100, 25);
+    createPlatform(700, 555, 100, 200);
+     createPlatform (1100, 650, 100, 25);
+      createPlatform (1300, 525, 100, 25);
+       createPlatform (1100, 425, 100, 25);
+       createPlatform (1300, 325, 100, 25);
+       createPlatform (1100, 225, 100, 25);
+       createPlatform (900, 225, 100, 25);
+       createPlatform (700, 225, 100, 25);
+       createPlatform (500, 225, 100, 25);
+       createPlatform (300, 225, 100, 25);
+       
 
     // TODO 3 - Create Collectables
+    createCollectable("hallejr", 1125, 170);
+    createCollectable("hallejr", 650, 700);
+     createCollectable("hallejr", 325, 175);
 
 
 
-    
+  
     // TODO 4 - Create Cannons
-
-
+    //createCannon("top / right / bottom / left", position, Speed);
+createCannon("top",200,1750);
+createCannon("top",510,1750);
+createCannon("top",710,1750);
+createCannon("top",910,1750);
+createCannon("top",1110,1750);
     
-    
+
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
